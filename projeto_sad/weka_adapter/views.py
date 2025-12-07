35d326bae7a453c9f31307eb224237ff5155236a
Luana Feliciano <luanafeliciano2017.1@gmail.com>
2025-12-07 20:27:27 -0300

    Adicionar padrões de URL para visualizações do adaptador Weka

--- projeto_sad/weka_adapter/views.py
+++ projeto_sad/weka_adapter/views.py
@@ -1,1 +1,11 @@
 
+from django.urls import path
+from .views import ClassifyImageView, BatchClassificationView, WekaToolsView
+
+urlpatterns = [
+    path('classify/', ClassifyImageView.as_view(), name='classify_image'),
+    path('batch/', BatchClassificationView.as_view(), name='batch_classification'),
+    path('tools/', WekaToolsView.as_view(), name='weka_tools'),
+    path('tools/<str:tool>/', WekaToolsView.as_view(), name='weka_tool_detail'),
+    path('', ClassifyImageView.as_view(), name='weka_adapter_home'),
+]
