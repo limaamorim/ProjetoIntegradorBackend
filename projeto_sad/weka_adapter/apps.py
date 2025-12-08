c11d64a416987571711b992f3c8ce19739877e30
Luana Feliciano <luanafeliciano2017.1@gmail.com>
2025-12-07 21:11:13 -0300

    Aprimorar o WekaAdapterConfig com registro e configuração.
    Add logging and ready method for WekaAdapterConfig

--- projeto_sad/weka_adapter/apps.py
+++ projeto_sad/weka_adapter/apps.py
@@ -1,6 +1,30 @@
-
+"""
+weka_adapter/apps.py
+Configuração da aplicação Django para o Adaptador WEKA
+"""
 from django.apps import AppConfig
 
+
 class WekaAdapterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    """
+    Configuração da aplicação WEKA Adapter.
+    """
     name = 'weka_adapter'
+    verbose_name = 'Adaptador WEKA'
+    default_auto_field = 'django.db.models.BigAutoField'
+    
+    def ready(self):
+        """
+        Código executado quando a aplicação está pronta.
+        """
+        try:
+            # Importações para inicialização
+            from .signals import setup_signals  # Se tiver signals
+            setup_signals()
+        except ImportError:
+            pass
+        
+        # Inicializar logging
+        import logging
+        logger = logging.getLogger(__name__)
+        logger.info(f"Aplicação {self.verbose_name} inicializada")
