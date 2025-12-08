748147132873a0b36165ac12160c1f6b7a0965c6
Luana Feliciano <luanafeliciano2017.1@gmail.com>
2025-12-07 21:06:42 -0300

    site administrativo personalizado para o adaptador WEKA.
    Added custom admin site configuration for WEKA Adapter.

--- projeto_sad/weka_adapter/admin.py
+++ projeto_sad/weka_adapter/admin.py
@@ -1,2 +1,18 @@
+"""
+weka_adapter/admin.py
+Configuração do Admin para o Adaptador WEKA
+"""
 from django.contrib import admin
-# Registrar modelos aqui
+from .models import ClassificationResult
+
+# Se você tiver um modelo, registre-o aqui
+# admin.site.register(ClassificationResult)
+
+# Ou configure uma página admin personalizada se necessário
+class WekaAdminSite(admin.AdminSite):
+    site_header = 'WEKA Adapter Admin'
+    site_title = 'WEKA Adapter'
+    index_title = 'Painel de Administração'
+
+# Se quiser criar um admin personalizado
+weka_admin = WekaAdminSite(name='weka_admin')
