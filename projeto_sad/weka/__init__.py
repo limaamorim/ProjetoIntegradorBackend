89cb00947d48e80b35019b5e5f713f4bbe860976
jonatanlimax <jonatanrobertodl@gmail.com>
2025-12-22 17:52:23 -0300

    Revert "Criar configuração weka pra Django -modulo"
    This reverts commit 2726e07190bd13f1a7b56903f33302c5d07720f1.

--- projeto_sad/weka/__init__.py
+++ projeto_sad/weka/__init__.py
@@ -1,10 +1,1 @@
-"""
-weka/apps.py
-Configuração do app Django para o módulo WEKA
-"""
-from django.apps import AppConfig
 
-class WekaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'weka'  # diagnostico_weka 
-    verbose_name = 'Sistema WEKA Simulado'  # weka_simulador
